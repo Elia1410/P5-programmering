8571220eb9059a53d71701eb35b127135f5082c4
Elias <eliasschack06@gmail.com>
2025-04-03 13:20:33 +0200

    simple scraper v2

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-URL = 'https://www.wwtbambored.com/viewtopic.php?f=1&t=62962'
+URL = 'https://www.wwtbambored.com/viewtopic.php?f=1&t=62961'
 page = requests.get(URL)
 
 scraper = bs(page.text, "html.parser")
 
-print(scraper.contents)
+scraperText = scraper.prettify().split("\n")
+scraperTextStripped = [line.strip() for line in scraperText]   
+
+for i, line in enumerate(scraperTextStripped):
+    if line[0] == "$":
+        print(scraperTextStripped[i+2])
+        print(scraperTextStripped[i+4])
+        print(scraperTextStripped[i+6])
+        nextLine =  i+8
+        while '<div class="spoiler-body">' not in scraperTextStripped[nextLine]:
+            nextLine += 1
+        print(scraperTextStripped[nextLine+1])
+        print("\n\n")
