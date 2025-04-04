32988783bed2a3e0338e1c2cc0e22ceca78db594
Elias <eliasschack06@gmail.com>
2025-04-04 21:55:03 +0200

    scraper v3: save single url content to json file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import json
 
 URL = 'https://www.wwtbambored.com/viewtopic.php?f=1&t=62961'
 page = requests.get(URL)
@@ -7,15 +8,62 @@ page = requests.get(URL)
 scraper = bs(page.text, "html.parser")
 
 scraperText = scraper.prettify().split("\n")
-scraperTextStripped = [line.strip() for line in scraperText]   
+scraperTextStripped = [line.strip() for line in scraperText]
+
+output = []
+
+def addQuestion(question: str, options: list, answer: str):
+    output.append({
+        "question": question,
+        "options": options,
+        "answer": options.index(answer[3:])
+    })
 
 for i, line in enumerate(scraperTextStripped):
-    if line[0] == "$":
-        print(scraperTextStripped[i+2])
-        print(scraperTextStripped[i+4])
-        print(scraperTextStripped[i+6])
-        nextLine =  i+8
-        while '<div class="spoiler-body">' not in scraperTextStripped[nextLine]:
-            nextLine += 1
-        print(scraperTextStripped[nextLine+1])
-        print("\n\n")
+    try:
+        if line[0] == "$":
+            question = scraperTextStripped[i+2]
+
+            optionsRaw = scraperTextStripped[i+4] + scraperTextStripped[i+6]
+            
+            a = ""
+            b = ""
+            c = ""
+            d = ""
+
+            cursor = 3
+            
+            while optionsRaw[cursor+3] != ":":
+                a += optionsRaw[cursor]
+                cursor += 1
+            cursor += 5
+            
+            while optionsRaw[cursor+1] != ":":
+                b += optionsRaw[cursor]
+                cursor += 1
+            cursor += 3
+            
+            while optionsRaw[cursor+3] != ":":
+                c += optionsRaw[cursor]
+                cursor += 1
+            cursor += 5
+
+            d = optionsRaw[cursor:]
+
+            nextLine =  i+8
+            while '<div class="spoiler-body">' not in scraperTextStripped[nextLine]:
+                nextLine += 1
+            
+            answer = scraperTextStripped[nextLine+1]
+
+            addQuestion(question, [a, b, c, d], answer)
+    except:
+        pass
+
+with open("output.json", "w") as file:
+    json.dump(output, file, indent=4)
+
+
+
+
+"A: Ghost hunting B: Duck huntingC: Bargain hunting D: Corporate headhunting"
