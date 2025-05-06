1f022fb1ccac54433e681446181de14e6d62cb96
Elias <eliasschack06@gmail.com>
2025-05-06 17:28:54 +0200

    in game text to speech implementeret itl oplæsning af spørgsmål

--- soundPlayer.py
+++ soundPlayer.py
@@ -1,11 +1,12 @@
 import pygame as pg
+import threading
 
 import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 rate = engine.getProperty('rate')
-engine.setProperty('rate', 125)
+engine.setProperty('rate', 150)
 
 class Sound:
     def __init__(self):
